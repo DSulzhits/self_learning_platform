ce38abfc54733de35933baee6374445d09f4493b
DSulzhits <dsulzhits@gmail.com>
2023-09-04 14:07:12 +0300

    add serializers for User views (добавлены сериализаторы для контроллеров User)

---
+++ users/serializers/user_serializers.py
@@ -0,0 +1,32 @@
+from abc import ABC
+
+from rest_framework.serializers import ModelSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from users.models import User
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class UserCreateSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class UserAuthSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class UserTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        return token
