21da25a1d263358065b5e380e9f96e9c9f974527
DSulzhits <dsulzhits@gmail.com>
2023-09-04 18:38:47 +0300

    Register models: Section, Content (Зарегистрированы модели: Section, Content)

--- sections/admin.py
+++ sections/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from sections.models import Section, Content
 
-# Register your models here.
+
+@admin.register(Section)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title',)
+    list_filter = ('id',)
+    ordering = ('id',)
+
+
+@admin.register(Content)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'section', 'title',)
+    list_filter = ('id', 'section',)
+    ordering = ('id', 'section',)
