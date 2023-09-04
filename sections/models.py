972492b0377840b3e61235a9600f60c22f8a4aee
DSulzhits <dsulzhits@gmail.com>
2023-09-04 18:31:51 +0300

    Add models Section, Content (Добавлены модели Section, Content)

--- sections/models.py
+++ sections/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Section(models.Model):
+    title = models.CharField(max_length=150, verbose_name="Title")
+    description = models.TextField(verbose_name="Description", null=True, blank=True)
+
+
+class Content(models.Model):
+    section = models.ForeignKey(Section, on_delete=models.CASCADE, verbose_name="Section")
+    title = models.CharField(max_length=150, verbose_name="Title")
+    content = models.TextField(verbose_name="Content")
