9bf93285fe26886771bcc5563f9215ddb375b894
DSulzhits <dsulzhits@gmail.com>
2023-09-04 14:09:07 +0300

    Model User registered in Admin (Модель User зарегистрирована в админке)

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from users.models import User
+
 
 # Register your models here.
+@admin.register(User)
+class CourseAdmin(admin.ModelAdmin):
+    """"""
+    list_display = ('id', 'email', 'is_active',)
+    list_filter = ('id', 'email',)
