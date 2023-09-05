e9500df491ba98c2cb31ec3cfd658365af8dbde5
DSulzhits <dsulzhits@gmail.com>
2023-09-05 20:42:12 +0300

    Update models Section, Content (Обновлены модели Section, Content)

--- sections/models.py
+++ sections/models.py
@@ -7,6 +7,13 @@ class Section(models.Model):
     title = models.CharField(max_length=150, verbose_name="Title")
     description = models.TextField(verbose_name="Description", null=True, blank=True)
 
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Section'
+        verbose_name_plural = 'Sections'
+
 
 class Content(models.Model):
     """Add Model Content
@@ -14,3 +21,10 @@ class Content(models.Model):
     section = models.ForeignKey(Section, on_delete=models.CASCADE, verbose_name="Section")
     title = models.CharField(max_length=150, verbose_name="Title")
     content = models.TextField(verbose_name="Content")
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Content'
+        verbose_name_plural = 'Content'
