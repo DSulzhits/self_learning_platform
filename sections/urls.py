ca4af566c0b3326836cf73c5ff6af7206c713fa5
DSulzhits <dsulzhits@gmail.com>
2023-09-11 14:55:25 +0300

    Add cache settings (Добавлены настройки кэша)

--- sections/urls.py
+++ sections/urls.py
@@ -4,6 +4,7 @@ from sections.apps import SectionsConfig
 from sections.views import SectionListAPIView, SectionCreateApiView, SectionRetrieveAPIView, SectionUpdateAPIView, \
     SectionDestroyAPIView, ContentListAPIView, ContentCreateApiView, ContentRetrieveAPIView, ContentUpdateAPIView, \
     ContentDestroyAPIView
+from django.views.decorators.cache import never_cache
 
 app_name = SectionsConfig.name
 
@@ -12,16 +13,16 @@ router = DefaultRouter()
 urlpatterns = [
     # Section urlpatterns
     path('section/', SectionListAPIView.as_view(), name='sections_list'),
-    path('section/create/', SectionCreateApiView.as_view(), name='section_create'),
+    path('section/create/', never_cache(SectionCreateApiView.as_view()), name='section_create'),
     path('section/<int:pk>/', SectionRetrieveAPIView.as_view(), name='section_detail'),
-    path('section/<int:pk>/update/', SectionUpdateAPIView.as_view(), name='section_update'),
-    path('section/<int:pk>/delete/', SectionDestroyAPIView.as_view(), name='section_delete'),
+    path('section/<int:pk>/update/', never_cache(SectionUpdateAPIView.as_view()), name='section_update'),
+    path('section/<int:pk>/delete/', never_cache(SectionDestroyAPIView.as_view()), name='section_delete'),
 
     # Content urlpatterns
     path('content/', ContentListAPIView.as_view(), name='content_list'),
-    path('content/create/', ContentCreateApiView.as_view(), name='content_create'),
+    path('content/create/', never_cache(ContentCreateApiView.as_view()), name='content_create'),
     path('content/<int:pk>/', ContentRetrieveAPIView.as_view(), name='content_detail'),
-    path('content/<int:pk>/update/', ContentUpdateAPIView.as_view(), name='content_update'),
-    path('content/<int:pk>/delete/', ContentDestroyAPIView.as_view(), name='content_delete'),
+    path('content/<int:pk>/update/', never_cache(ContentUpdateAPIView.as_view()), name='content_update'),
+    path('content/<int:pk>/delete/', never_cache(ContentDestroyAPIView.as_view()), name='content_delete'),
 
 ] + router.urls
--- users/urls.py
+++ users/urls.py
@@ -2,16 +2,17 @@ from django.urls import path
 from users.apps import UsersConfig
 from users.views import UserListAPIView, UserCreateAPIView, UserRetrieveAPIView, UserUpdateAPIView, UserDestroyAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.views.decorators.cache import never_cache
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     # user urlpatterns
     path('', UserListAPIView.as_view(), name='users_list'),
-    path('create/', UserCreateAPIView.as_view(), name='user_create'),
+    path('create/', never_cache(UserCreateAPIView.as_view()), name='user_create'),
     path('<int:pk>/', UserRetrieveAPIView.as_view(), name='user_detail'),
-    path('<int:pk>/update/', UserUpdateAPIView.as_view(), name='user_update'),
-    path('<int:pk>/delete/', UserDestroyAPIView.as_view(), name='user_delete'),
+    path('<int:pk>/update/', never_cache(UserUpdateAPIView.as_view()), name='user_update'),
+    path('<int:pk>/delete/', never_cache(UserDestroyAPIView.as_view()), name='user_delete'),
 
     # token urlpatterns
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
