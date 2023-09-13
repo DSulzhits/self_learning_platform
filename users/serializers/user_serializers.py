05d9668427fe660ccfd4dbd712d4f04283e15d69
DSulzhits <dsulzhits@gmail.com>
2023-09-13 16:14:49 +0300

    Update UserCreateSerializer (Обновлен UserCreateSerializer)

--- users/serializers/user_serializers.py
+++ users/serializers/user_serializers.py
@@ -16,7 +16,16 @@ class UserSerializer(ModelSerializer):
 class UserCreateSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['email', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        instance.is_active = True
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 
 class UserAuthSerializer(ModelSerializer):
