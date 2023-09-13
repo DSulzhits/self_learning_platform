2eb25dc220c9d40951f145907604c0095a5271af
DSulzhits <dsulzhits@gmail.com>
2023-09-13 12:18:29 +0300

    Add utils.py in \sections\tests\ (Добавлен utils.py в \sections\tests\)

---
+++ sections/tests/utils.py
@@ -0,0 +1,14 @@
+from users.models import User, UserRoles
+
+
+def get_user():
+    user = User.objects.create(
+        email='tester@test1.com',
+        role=UserRoles.MODERATOR,
+        is_active=True,
+        is_superuser=True,
+        is_staff=True
+    )
+    user.set_password('qwerty')
+    user.save()
+    return user
