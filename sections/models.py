01a41685578dfe28b4a55096bc94e42cdabb4973
DSulzhits <dsulzhits@gmail.com>
2023-09-15 18:49:49 +0300

    Update code (Обновление кода)

--- sections/models.py
+++ sections/models.py
@@ -34,15 +34,15 @@ class Content(models.Model):
 
 
 class Tests(models.Model):
-    section = models.ForeignKey(Section, on_delete=models.CASCADE, verbose_name="Section")
+    test_section = models.ForeignKey(Section, on_delete=models.CASCADE, verbose_name="Test section")
     description = models.TextField(verbose_name='Test description', **NULLABLE)
     question = models.TextField(verbose_name='Question', **NULLABLE)
     answer = models.CharField(max_length=40, verbose_name='Answer', **NULLABLE)
 
     def __str__(self):
-        return f'Тест по курсу {self.section.title}'
+        return f'Тест по курсу {self.test_section.title}'
 
     class Meta:
         verbose_name = 'Test'
         verbose_name_plural = 'Tests'
-        ordering = ['section']
+        ordering = ['test_section']
--- sections/serializers/tests_serializers.py
+++ sections/serializers/tests_serializers.py
@@ -2,21 +2,16 @@ from rest_framework.serializers import ModelSerializer
 from sections.models import Tests, Section
 from rest_framework.relations import SlugRelatedField
 
+
 class TestsSerializer(ModelSerializer):
+    test_section = SlugRelatedField(slug_field="title", queryset=Section.objects.all())
+
     class Meta:
         model = Tests
-        fields = '__all__'
-
-
-class TestsSectionSerializer(ModelSerializer):
-    section = SlugRelatedField(slug_field="title", queryset=Section.objects.all())
-    class Meta:
-        model = Tests
-        fields = ('id', 'section',)
+        fields = ('id', 'test_section',)
 
 
 class TestsQuestionSerializer(ModelSerializer):
     class Meta:
         model = Tests
-        fields = ('id', 'section', 'question', 'answer',)
-
+        fields = ('id', 'test_section', 'question', 'answer',)
