230b5c14d0cb0f4061135fff66780639c2a14ac7
DSulzhits <dsulzhits@gmail.com>
2023-09-15 12:59:39 +0300

    Update UserCreateSerializer (Обновлен UserCreateSerializer)

--- users/serializers/user_serializers.py
+++ users/serializers/user_serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from users.models import User
@@ -7,25 +7,24 @@ from users.models import User
 (Добавлены сериализаторы для контроллеров User)"""
 
 
-class UserSerializer(ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
 
 
-class UserCreateSerializer(ModelSerializer):
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, min_length=6, max_length=12)
+
     class Meta:
         model = User
         fields = ['email', 'password']
 
     def create(self, validated_data):
-        password = validated_data.pop('password', None)
-        instance = self.Meta.model(**validated_data)
-        instance.is_active = True
-        if password is not None:
-            instance.set_password(password)
-        instance.save()
-        return instance
+        user = User.objects.create(**validated_data)
+        user.set_password(user.password)
+        user.save()
+        return user
 
 
 class UserTokenObtainPairSerializer(TokenObtainPairSerializer):
