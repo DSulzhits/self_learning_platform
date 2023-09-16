d4752d2a4366ad4ce82f3d3d56e0c9308e2363e2
DSulzhits <dsulzhits@gmail.com>
2023-09-16 11:38:07 +0300

    Add SlugRelatedField in TestsQuestionSerializer (Добавлено SlugRelatedField в TestsQuestionSerializer)

--- sections/serializers/tests_serializers.py
+++ sections/serializers/tests_serializers.py
@@ -12,6 +12,8 @@ class TestsSerializer(ModelSerializer):
 
 
 class TestsQuestionSerializer(ModelSerializer):
+    test_section = SlugRelatedField(slug_field="title", queryset=Section.objects.all())
+
     class Meta:
         model = Tests
         fields = ('id', 'test_section', 'question', 'answer',)
