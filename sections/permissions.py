675aef675f2848cfa1185f95b8d69dc3db4bd7bd
DSulzhits <dsulzhits@gmail.com>
2023-09-21 09:31:44 +0300

    Create custom permission IsSuperuser (Создано дополнительное ограничение IsSuperuser)

--- sections/permissions.py
+++ sections/permissions.py
@@ -11,3 +11,12 @@ class IsModerator(BasePermission):
         if request.user.role == UserRoles.MODERATOR:
             return True
         return False
+
+
+class IsSuperuser(BasePermission):
+    message = "Только для администраторов ресурса"
+
+    def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
+        return False
